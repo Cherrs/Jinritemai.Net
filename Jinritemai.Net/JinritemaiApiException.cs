1dadaa3e4b71f45bf601e5ffd526955f3c8206b6
KuriyamaMirai <lovef@outlook.com>
2023-07-27 15:26:30 +0800

    format

--- Jinritemai.Net/JinritemaiApiException.cs
+++ Jinritemai.Net/JinritemaiApiException.cs
@@ -1,11 +1,13 @@
 using System;
 using Jinritemai.Net;
 
-namespace Jinritemai.Net;
-
-public class JinritemaiApiException<T> : Exception
+namespace Jinritemai.Net
 {
-    public JinritemaiApiException(Result<T> Result) : base(Result.msg)
+    public class JinritemaiApiException<T> : Exception
     {
+        public JinritemaiApiException(Result<T> Result) : base(Result.msg)
+        {
+        }
     }
 }
+
